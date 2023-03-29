38c6c66eec22a911820e310609fea3c0421410c2
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-29 11:13:57 +0300

    Proje mimarisi kuruldu...Entities katmanı tamamlandı

---
+++ Project.ENTITIES/Models/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using Project.ENTITIES.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class AppUser : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Guid ActivationCode { get; set; } //Kullanıcıya üyelik onayı icin gönderilen unique bir sifreleme tipi (Guid)
+        public bool Active { get; set; }
+        public string Email { get; set; }
+        public UserRole Role { get; set; }
+
+        //Relational Properties
+        public virtual AppUserProfile Profile { get; set; }
+        public virtual List<Order> Orders { get; set; }
+
+
+    }
+}
