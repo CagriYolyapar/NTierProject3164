c253a285b030db255f483b294fdc4d57d04ab320
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-30 12:18:56 +0300

    Areas acıldı...Layout entegre edildi

---
+++ Project.MVCUI/Models/ShoppingTools/CartItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.MVCUI.Models.ShoppingTools
+{
+    public class CartItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public short Amount { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+        
+        public decimal SubTotal
+        {
+            get
+            {
+                return Price * Amount;
+            }
+        }
+
+        public CartItem()
+        {
+            Amount++;
+        }
+    }
+}
